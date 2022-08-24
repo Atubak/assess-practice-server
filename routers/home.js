6b3eee85a68ab4b87aabd9710108283469e139f4
Jona <mr-jona@hotmail.com>
2022-08-24 18:14:11 +0200

    home route sends a list of spaces

--- routers/home.js
+++ routers/home.js
@@ -1,9 +1,22 @@
 const { Router } = require("express");
 
+//models
+const spaceModel = require("../models").space;
+
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
-  res.send("lallalalala");
+  try {
+    const allSpaces = await spaceModel.findAll();
+
+    if (!allSpaces) {
+      res.status(404).send("no spaces found!");
+    }
+
+    res.json(allSpaces);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
