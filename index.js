dab0fc0073b6c54e468b5d3d3b4faad6aa294397
Jona <mr-jona@hotmail.com>
2022-08-24 16:27:53 +0200

    start a new route endpoint

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const corsMiddleWare = require("cors");
 
 //routers
 const authRouter = require("./routers/auth");
+const homeRouter = require("./routers/home");
 
 //constants
 const { PORT } = require("./config/constants");
 
 // Create an express app
 const app = express();
-
 
 // CORS middleware:  * Since our api is hosted on a different domain than our client
 // we are are doing "Cross Origin Resource Sharing" (cors)
@@ -20,8 +20,8 @@ app.use(corsMiddleWare());
 // express.json() to be able to read request bodies of JSON requests a.k.a. body-parser
 app.use(express.json());
 
-
 //routes
+app.use("/", homeRouter);
 app.use("/auth", authRouter);
 
 //start listening
